2b3c6940a82f20c941720e67188b65387a50fb32
Timothy Laurent <timothyjlaurent@gmail.com>
2013-12-10 06:20:09 +0000

    stuck at the configure step

--- src/ports/BLAST/build.sh
+++ src/ports/BLAST/build.sh
@@ -2,5 +2,63 @@
 source pkg_info
 source ../../build_tools/common.sh
 
+
+DefaultConfigureStep() {
+  local EXTRA_CONFIGURE_OPTS=("${@:-}")
+  Banner "Configuring ${PACKAGE_NAME}"
+  # export the nacl tools
+  export CC=${NACLCC}
+  export CXX=${NACLCXX}
+  export AR=${NACLAR}
+  export RANLIB=${NACLRANLIB}
+  export READELF=${NACLREADELF}
+  export PKG_CONFIG_PATH=${NACLPORTS_LIBDIR}/pkgconfig
+  export PKG_CONFIG_LIBDIR=${NACLPORTS_LIBDIR}
+  export FREETYPE_CONFIG=${NACLPORTS_PREFIX_BIN}/freetype-config
+  export CFLAGS=${NACLPORTS_CFLAGS}
+  export CXXFLAGS=${NACLPORTS_CXXFLAGS}
+  export LDFLAGS=${NACLPORTS_LDFLAGS}
+  export PATH=${NACL_BIN_PATH}:${PATH};
+  local SRC_DIR=${NACL_PACKAGES_REPOSITORY}/${PACKAGE_DIR}
+  local CONFIGURE=${NACL_CONFIGURE_PATH:-${SRC_DIR}/c++/configure}
+  if [ ! -f "${CONFIGURE}" ]; then
+    echo "No configure script found at ${CONFIGURE}"
+    return
+  fi
+  local DEFAULT_BUILD_DIR=${SRC_DIR}/${NACL_BUILD_SUBDIR}
+  NACL_BUILD_DIR=${NACL_BUILD_DIR:-${DEFAULT_BUILD_DIR}}
+  MakeDir ${NACL_BUILD_DIR}
+  ChangeDir ${NACL_BUILD_DIR}
+
+  local conf_host=${NACL_CROSS_PREFIX}
+  if [ "${NACL_ARCH}" = "pnacl" -o "${NACL_ARCH}" = "emscripten" ]; then
+    # The PNaCl tools use "pnacl-" as the prefix, but config.sub
+    # does not know about "pnacl".  It only knows about "le32-nacl".
+    # Unfortunately, most of the config.subs here are so old that
+    # it doesn't know about that "le32" either.  So we just say "nacl".
+    conf_host="nacl"
+  fi
+
+  if [ -n "${CONFIGURE_SENTINEL:-}" -a -f "${CONFIGURE_SENTINEL:-}" ]; then
+    return
+  fi
+
+  LogExecute ${CONFIGURE} \
+    --host=${conf_host} \
+    --prefix=${NACLPORTS_PREFIX} \
+    --exec-prefix=${NACLPORTS_PREFIX} \
+    --libdir=${NACLPORTS_LIBDIR} \
+    --oldincludedir=${NACLPORTS_INCLUDE} \
+    --with-http=no \
+    --with-html=no \
+    --with-ftp=no \
+    --${NACL_OPTION}-mmx \
+    --${NACL_OPTION}-sse \
+    --${NACL_OPTION}-sse2 \
+    --${NACL_OPTION}-asm \
+    --with-x=no  \
+    "${EXTRA_CONFIGURE_OPTS[@]}" ${EXTRA_CONFIGURE_ARGS:-}
+}
+
 PackageInstall
 exit 0
